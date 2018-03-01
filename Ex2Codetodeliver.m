674ed0a2c900f270c6d7148552cce08a88831178
filipposbete <36484002+filipposbete@users.noreply.github.com>
2018-03-01 20:07:11 +0200

    Update Ex2Codetodeliver.m

--- Ex2Codetodeliver.m
+++ Ex2Codetodeliver.m
@@ -1,33 +1,35 @@
-%My data
+%Loading my data
 load('DATAFinanceExercise2.mat')
 load('DATAIndustriesNames.mat');
 
-%We rename the Matrix AverageValueWeightedReturnsMontly to A
-A = AverageValueWeightedReturnsMonthly;
+%We rename the Matrix AverageValueWeightedReturnsMontly to 'MonthlyReturns'
+MonthlyReturns = AverageValueWeightedReturnsMonthly;
 
-%We name the Mean Matrix = M and C = Variance-Covariance Matrix
-M = nanmean(A);
-C = cov( A ,'partialrows');
+%We name the Mean Matrix = Mean and VarCovar = Variance-Covariance Matrix
+Mean = nanmean(MonthlyReturns);
+VarCovar = cov(MonthlyReturns, 'partialrows');
 
 %Efficient Frontier and the optimal Sharpe ratio
-p = Portfolio('AssetMean',M , 'AssetCovar', C, 'LowerBound', 0,'UpperBound',1, 'Budget',1);
-plotFrontier(p);
+Portf = Portfolio('AssetMean', Mean, 'AssetCovar', VarCovar, 'LowerBound', 0, 'UpperBound', 1, 'Budget', 1);
+plotFrontier(Portf);
 figure;
-plotFrontier(p);
-portweights = estimateMaxSharpeRatio(p);
-[risk, ret] = estimatePortMoments(p, portweights);
+plotFrontier(Portf);
+Weights = estimateMaxSharpeRatio(Portf);
+[risk, ret] = estimatePortMoments(Portf, Weights);
 hold on
 plot(risk,ret,'*k');
 title('Efficient Frontier and Sharpe Ratio');
-sum(portweights);
+SumOfWeights = sum(Weights);
 
-%Create a pie to present the weights.
-k=reshape(portweights,1,49);
+%Presentation of weights, using pie.
+TransposeWeights = reshape(Weights, 1, 49);
 figure;
-pie(k);
-legend('Agric','Food','Soda','Beer','Smoke','Toys','Fun','Books','Hshld','Clths','Hlth','MedEq','Drugs','Chems','Rubbr','Txtls','BldMt','Cnstr','Steel',...
-'FabPr','Mach','ElcEq','Autos','Aero','Ships','Guns','Gold','Mines','Coal','Oil','Util','Telcm','PerSv','BusSv','Hardw','Softw','Chips','LabEq','Paper','Boxes',...
-'Trans','Whlsl','Rtail','Meals','Banks','Insur','RIEst','Fin','Other');
+pie(TransposeWeights);
+legend('Agric','Food','Soda','Beer','Smoke','Toys','Fun','Books','Hshld',...
+       'Clths','Hlth','MedEq','Drugs','Chems','Rubbr','Txtls','BldMt','Cnstr','Steel',...
+       'FabPr','Mach','ElcEq','Autos','Aero','Ships','Guns','Gold','Mines','Coal','Oil','Util',...
+       'Telcm','PerSv','BusSv','Hardw','Softw','Chips','LabEq','Paper','Boxes',...
+       'Trans','Whlsl','Rtail','Meals','Banks','Insur','RIEst','Fin','Other');
 
 
 
